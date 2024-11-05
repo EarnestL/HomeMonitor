99ae3c3e72c96f226baadb69d0b06aa973e0c813
earnestlin123 <120564434+EarnestL@users.noreply.github.com>
2024-11-05 01:08:03 -0800

    cors added

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
 // app.js
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const tempRoutes = require('./routes/tempRoutes');
 const humRoutes = require('./routes/humRoutes');
 
+
 const app = express();
 
+app.use(cors());
 // Middleware
 app.use(bodyParser.json());
 
